3aa69d388e3a08b1f4cb4f1f4e5a2c5df5ffcb3e
王毛孩 <wanglui1990@hotmail.com>
2018-01-26 17:31:47 +0800

    Angular HttpClient get、put、patch、delete、post示例

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,120 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from "rxjs/Observable";
+import { HttpClient, } from "@angular/common/http";
+import { HttpParams } from "@angular/common/http";
+import { HttpHeaders } from '@angular/common/http';
 
+class Customer {
+  id: number;
+  name: string;
+  email: string;
+  tel: string;
+}
+/*
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+*/
+@Component({
+  selector: 'customers',
+  templateUrl: './app.component.html'
+}
+)
+
+export class AppComponent implements OnInit {
+  customersObservable: Observable<Customer[]>;
+
   title = 'app';
+  constructor(private httpClient: HttpClient) {
+  }
+  ngOnInit() {
+     /*this.customersObservable = this.httpClient
+      .get<Customer[]>("http://127.0.0.1:3000/customers");*/
+}
+  getWithNoPara()
+  {
+    this.customersObservable = this.httpClient
+      .get<Customer[]>("http://127.0.0.1:3000/customers");
+  }
+  getWithpara()
+  {
+        const params = new HttpParams().set('_page', "1").set('_limit', "1");
+        this.customersObservable = this.httpClient
+          .get<Customer[]>("http://127.0.0.1:3000/customers",{ params });
+  }
+  getWithParaFromString()
+  {
+        const params = new HttpParams({fromString: '_page=1&_limit=1'});
+        this.customersObservable = this.httpClient
+          .get<Customer[]>("http://127.0.0.1:3000/customers",{ params });
+  }
+  getWithHttpHeader()
+  {
+    const headers = new HttpHeaders().set("X-CustomHttpHeader", "CUSTOM_VALUE");
+		this.customersObservable = this.httpClient.get<Customer[]>("http://127.0.0.1:3000/customers", {headers});
+  
+  }
+  put()
+  {
+    this.httpClient.put("http://127.0.0.1:3000/customers/1",
+    {
+        "name": "NewCustomer001",
+        "email": "newcustomer001@email.com",
+        "tel": "0000252525"
+    })
+    .subscribe(
+        data => {
+            console.log("PUT Request is successful ", data);
+        },
+        error => {
+            console.log("Rrror", error);
+        }
+    );
+  }
+  patch()
+  {
+    this.httpClient.patch("http://127.0.0.1:3000/customers/1",
+    {
+        "email": "newcustomer001@gmail.com"
+    })
+    .subscribe(
+        data => {
+            console.log("PUT Request is successful ", data);
+        },
+        error => {
+            console.log("Error", error);
+        }
+    );  
+  }
+  delete()
+  {
+    this.httpClient.delete("http://127.0.0.1:3000/customers/1")
+    .subscribe(
+        data => {
+            console.log("DELETE Request is successful ", data);
+        },
+        error => {
+            console.log("Error", error);
+        }
+    ); 
+  }
+  post()
+  {
+    this.httpClient.post("http://127.0.0.1:3000/customers",
+    {
+        "name": "Customer004",
+        "email": "customer004@email.com",
+        "tel": "0000252525"
+    })
+    .subscribe(
+        data => {
+            console.log("POST Request is successful ", data);
+        },
+        error => {
+            console.log("Error", error);
+        }
+    );           
+  }
 }
